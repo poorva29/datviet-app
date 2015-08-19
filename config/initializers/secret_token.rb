2f375903033d2f7c2c03251c3c5f53790eb0873a
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2015-08-19 11:46:05 +0530

    Initialised the app with dashboard controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DatvietApp::Application.config.secret_key_base = 'b4e12330ecb97260227f63870253b9caa0932b0d2503440e2c0d570120458854f8bfa071562cd2ae38757750e2480529d50af31c118e7e66cfee0829e7314597'
